80dd7f855ac81caed79face35b9c42aad334f69a
Przemek <przemek6051@gmail.com>
2025-12-07 11:24:25 +0000

    changes file

--- file.py
+++ file.py
@@ -1,7 +1,7 @@
-x = 5
+N = 10
+total = 0
 
-for i in range(10):
-    x = x + i
+for i in range(1, N + 1):
+    total += i  # same as total = total + i
 
-print(x)  # optional, to see the result
-          
+print("Sum from 1 to", N, "is", total)
